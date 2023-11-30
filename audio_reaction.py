eaaa228bc5150a2f1a499de7800cff23c3ce3b81
gcausey <143149041+gcausey@users.noreply.github.com>
2023-11-30 13:55:48 -0500

    audio script

--- audio_reaction.py
+++ audio_reaction.py
@@ -1,4 +1,4 @@
-import pygame
+#import pygame
 import math
 import pyaudio
 import time
@@ -14,72 +14,17 @@ LED_BRIGHTNESS = 65      # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-# Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
+def pulse(strip, color, pulse_duration=5, steps=100):
+    """Creates a pulsing effect by gradually increasing and decreasing brightness."""
+    for i in range(steps):
+        brightness = int((1 - abs((i % (steps // 2)) - (steps // 4)) / (steps // 4)) * 255)
+        for j in range(strip.numPixels()):
+            strip.setPixelColor(j, Color(int(color >> 16) * brightness // 255, int((color >> 8) & 0xFF) * brightness />
         strip.show()
-        time.sleep(wait_ms/1000.0)
-
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-    """Movie theater light style chaser animation."""
-    for j in range(iterations):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, color)
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
-
-def wheel(pos):
-    """Generate rainbow colors across 0-255 positions."""
-    if pos < 85:
-        return Color(pos * 3, 255 - pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return Color(255 - pos * 3, 0, pos * 3)
-    else:
-        pos -= 170
-        return Color(0, pos * 3, 255 - pos * 3)
-
-def rainbow(strip, wait_ms=20, iterations=1):
-    """Draw rainbow that fades across all pixels at once."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((i+j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-    """Draw rainbow that uniformly distributes itself across all pixels."""
-    for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-        strip.show()
-        time.sleep(wait_ms/1000.0)
-
-def theaterChaseRainbow(strip, wait_ms=50):
-    """Rainbow movie theater light style chaser animation."""
-    for j in range(256):
-        for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, wheel((i+j) % 255))
-            strip.show()
-            time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
-                strip.setPixelColor(i+q, 0)
-
+        time.sleep(pulse_duration / steps)
 
 # Initialize Pygame
-pygame.init()
-
-#screen setup
-screen_width = 500
-screen_height = 500
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Audio Reaction Test")
+#pygame.init()
 
 #Audio variable initialization
 CHUNK = 1024
@@ -118,30 +63,29 @@ def draw_light_map(amplitude):
 
 
 #Clock Setup
-clock = pygame.time.Clock()
+#clock = pygame.time.Clock()
 
 running = True
 amplitude = 100
 
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+# Intialize the library (must be called once before other functions).
+strip.begin()
 
 while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+   # for event in pygame.event.get():
+       # if event.type == pygame.QUIT:
+           # running = False
 
     amplitude_adjustment = get_microphone_input_level() / 50
     amplitude  = max(10, amplitude_adjustment)
-     # Create NeoPixel object with appropriate configuration.
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    # Intialize the library (must be called once before other functions).
-    strip.begin()
-
 
     draw_light_map(amplitude)
     #print(get_microphone_input_level())
     
     #frame rate cap
-    clock.tick(60)
+#    clock.tick(60)
 
-pygame.quit()
+#pygame.quit()
 
